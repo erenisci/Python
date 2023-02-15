e00cdd14d1449ba66ffb32ffeab9d4a28cffe910
Eren İşci <isci.eren@gmail.com>
2023-02-15 09:32:39 +0300

    Workspace-3

--- Workspace-3/07_inheritance.py
+++ Workspace-3/07_inheritance.py
@@ -29,53 +29,19 @@ class Programmer(Employee):
         return f"Language: {self.lang}"
 
 
-class Manager(Employee):
-    def __init__(self, name, surname, salary, worker=None):
-        super().__init__(name, surname, salary)
-        if worker == None:
-            self.worker = []
-        else:
-            self.worker = worker
-
-    def add_worker(self, worker):
-        if worker not in self.worker:
-            self.worker.append(worker)
-
-    def del_worker(self, worker):
-        if worker in self.worker:
-            self.worker.remove(worker)
-
-    def show_worker(self):
-        for worker in self.worker:
-            print(worker.info())
-
-
 employee1 = Employee("Eren", "Isci", 5000)
 employee2 = Employee("Alice", "Parker", 4900)
 
 programmer1 = Programmer("Selin", "Isci", 7000, "Python")
 programmer2 = Programmer("John", "Hank", 8000, "Java")
 
-manager1 = Manager("Steve", "Johnson", 10000)
+print(employee1.pay_rate)
+print(programmer1.pay_rate)
 
-print("\n*************************")
-manager1.add_worker(programmer1)
-manager1.add_worker(programmer2)
-manager1.show_worker()
+print(employee1.info())
+print(employee2.info())
 
-print("\n*************************")
-manager1.del_worker(programmer1)
-manager1.show_worker()
+print(programmer1.info())
+print(programmer2.info())
 
-print("\n*************************")
-print(isinstance(manager1, Manager))
-manager2 = Manager("Tim", "Rogers", 10000, [manager1])
-manager2.show_worker()
-
-print("\n*************************")
-print(isinstance(manager1, Employee))
-print(isinstance(manager1, Manager))
-
-print("\n*************************")
-print(issubclass(Programmer, Employee))
-print(issubclass(Employee, Programmer))
+print(programmer1.info_lang())
