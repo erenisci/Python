64517c59c201bf16d6fb0806f57c884e96aefec8
Eren İşci <isci.eren@gmail.com>
2023-08-07 12:10:25 +0300

    5-Workspace

---
+++ 5-Workspace/5_zip_func.py
@@ -0,0 +1,12 @@
+lis = [1, 2, 3, 4]
+lis2 = ["a", "b", "c", "d"]
+
+result = list(zip(lis, lis2))
+print(result)
+
+
+lis3 = list("Python")
+lis4 = [1, 2, 3, 4]
+lis5 = ["A", "B", "C"]
+lis6 = list(zip(lis3, lis4, lis5))
+print(lis6)
